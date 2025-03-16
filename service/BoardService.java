2ec913fbd7dd392f4439595fd0c91f49e288accb
ivandronovais <ivandronovais@gmail.com>
2025-03-16 19:29:06 -0300

    🔧 chore(launch): add runtime arguments for board initialization

--- service/BoardService.java
+++ service/BoardService.java
@@ -53,4 +53,5 @@ public class BoardService {
 
         return spaces;
     }
+    
 }
--- ui/custom/screen/MainScreen.java
+++ ui/custom/screen/MainScreen.java
@@ -2,7 +2,6 @@ package ui.custom.screen;
 
 import model.Space;
 import service.BoardService;
-import service.EventEnum;
 import service.NotifierService;
 import ui.custom.button.CheckGameStatusButton;
 import ui.custom.button.FinishGameButton;
@@ -14,7 +13,6 @@ import ui.custom.panel.SudokuSector;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import java.awt.Dimension;
 import java.util.ArrayList;
@@ -43,12 +41,12 @@ public class MainScreen {
         this.notifierService = new NotifierService();
     }
 
-    public void buildMainScreen(){
+    public void buildMainScreen() {
         JPanel mainPanel = new MainPanel(dimension);
         JFrame mainFrame = new MainFrame(dimension, mainPanel);
-        for (int r = 0; r < 9; r+=3) {
+        for (int r = 0; r < 9; r += 3) {
             var endRow = r + 2;
-            for (int c = 0; c < 9; c+=3) {
+            for (int c = 0; c < 9; c += 3) {
                 var endCol = c + 2;
                 var spaces = getSpacesFromSector(boardService.getSpaces(), c, endCol, r, endRow);
                 JPanel sector = generateSection(spaces);
@@ -63,8 +61,8 @@ public class MainScreen {
     }
 
     private List<Space> getSpacesFromSector(final List<List<Space>> spaces,
-                                            final int initCol, final int endCol,
-                                            final int initRow, final int endRow){
+            final int initCol, final int endCol,
+            final int initRow, final int endRow) {
         List<Space> spaceSector = new ArrayList<>();
         for (int r = initRow; r <= endRow; r++) {
             for (int c = initCol; c <= endCol; c++) {
@@ -74,7 +72,7 @@ public class MainScreen {
         return spaceSector;
     }
 
-    private JPanel generateSection(final List<Space> spaces){
+    private JPanel generateSection(final List<Space> spaces) {
         List<NumberText> fields = new ArrayList<>(spaces.stream().map(NumberText::new).toList());
         fields.forEach(t -> notifierService.subscribe(CLEAR_SPACE, t));
         return new SudokuSector(fields);
@@ -82,7 +80,7 @@ public class MainScreen {
 
     private void addFinishGameButton(final JPanel mainPanel) {
         finishGameButton = new FinishGameButton(e -> {
-            if (boardService.gameIsFinished()){
+            if (boardService.gameIsFinished()) {
                 showMessageDialog(null, "Parabéns você concluiu o jogo");
                 resetButton.setEnabled(false);
                 checkGameStatusButton.setEnabled(false);
@@ -99,7 +97,7 @@ public class MainScreen {
         checkGameStatusButton = new CheckGameStatusButton(e -> {
             var hasErrors = boardService.hasErrors();
             var gameStatus = boardService.getStatus();
-            var message = switch (gameStatus){
+            var message = switch (gameStatus) {
                 case NON_STARTED -> "O jogo não foi iniciado";
                 case INCOMPLETE -> "O jogo está imcompleto";
                 case COMPLETE -> "O jogo está completo";
@@ -111,15 +109,14 @@ public class MainScreen {
     }
 
     private void addResetButton(final JPanel mainPanel) {
-        resetButton = new ResetButton(e ->{
+        resetButton = new ResetButton(e -> {
             var dialogResult = showConfirmDialog(
                     null,
                     "Deseja realmente reiniciar o jogo?",
                     "Limpar o jogo",
                     YES_NO_OPTION,
-                    QUESTION_MESSAGE
-            );
-            if (dialogResult == 0){
+                    QUESTION_MESSAGE);
+            if (dialogResult == 0) {
                 boardService.reset();
                 notifierService.notify(CLEAR_SPACE);
             }
